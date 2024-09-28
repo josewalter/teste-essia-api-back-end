9cc4e78cf0e610a61cdf2ad5f9599679ab582243
josewalter <josewalter408@gmail.com>
2024-09-28 11:40:16 -0300

    Começando a preparar as classes de teste

--- src/main/java/com/testessiaapi/dto/request/ClienteRecordDto.java
+++ src/main/java/com/testessiaapi/dto/request/ClienteRecordDto.java
@@ -2,5 +2,6 @@ package com.testessiaapi.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record ClienteRecordDto (@NotBlank String nomeCliente, @NotBlank String cpf, @NotBlank String email, @NotBlank String telefoneCelular){
+public record ClienteResponseDto(@NotBlank String nomeCliente, @NotBlank String cpf, @NotBlank String email, @NotBlank String telefoneCelular,
+                                 String s){
 }
